773cb0771a4e417093f533892d28a2a6664d07da
Luis Novo <luis.novo@gmail.com>
2023-04-21 19:48:48 +0200

    Changed p10 context color

--- files/p10k.zsh
+++ files/p10k.zsh
@@ -820,11 +820,10 @@
 
   ##################################[ context: user@hostname ]##################################
   # Context color when running with privileges.
-  typeset -g POWERLEVEL9K_CONTEXT_ROOT_FOREGROUND=178
+  typeset -g POWERLEVEL9K_CONTEXT_ROOT_FOREGROUND=244
   # Context color in SSH without privileges.
   typeset -g POWERLEVEL9K_CONTEXT_{REMOTE,REMOTE_SUDO}_FOREGROUND=180
   # Default context color (no privileges, no SSH).
-  #typeset -g POWERLEVEL9K_CONTEXT_FOREGROUND=180
   typeset -g POWERLEVEL9K_CONTEXT_FOREGROUND=244
 
   # Context format when running with privileges: bold user@hostname.
